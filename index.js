de910fe3395a486c5ec0f715dfa408d2916b3218
TechBree <bridget_kodohu@yahoo.com>
2024-11-07 19:07:09 +0100

    Removed the city from HTML but loaded through JS

--- index.js
+++ index.js
@@ -21,3 +21,5 @@ function handleSubmitSearch(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSubmitSearch);
+
+searchCity("Warsaw");
