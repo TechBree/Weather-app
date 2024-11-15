29306fb5858a569ff421769f918989393d60426f
TechBree <bridget_kodohu@yahoo.com>
2024-11-15 23:15:18 +0100

    Activate a ready made code on my project

--- index.js
+++ index.js
@@ -51,6 +51,47 @@ function handleSubmitSearch(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "bbfa370443fe49feaa41t631d604c6oa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function showForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+        <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather -forecast-temperatures">
+        <div class="weather-forecast-temperature">
+        <strong>${Math.round(day.temperature.maximum)}º</strong>
+        </div>
+        <div class="weather-forecast-temperature">${Math.round(
+          day.temperature.minimum
+        )}º</div>
+        </div>
+        </div>
+        `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSubmitSearch);
 
