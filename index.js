6f98adc2fab80359367bae91d399d28ec421745b
TechBree <bridget_kodohu@yahoo.com>
2024-11-16 11:30:46 +0100

    correcting the function name

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function formatDay(timestamp) {
 function getForecast(city) {
   let apiKey = "bbfa370443fe49feaa41t631d604c6oa";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  axios(apiUrl).then(displayForecast);
+  axios(apiUrl).then(showForecast);
 }
 
 function showForecast(response) {
